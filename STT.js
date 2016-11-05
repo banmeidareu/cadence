dacebaac353208acca37cab03f10682e84ff4b68
Jiachen Wang <Jiachen775@gmail.com>
2016-11-05 17:54:31 -0400

    stt.out with address

--- STT.js
+++ STT.js
@@ -1,5 +1,5 @@
-record() {
-beep = true,
+record({
+    beep = true,
     // probably don't need this: recordURI: "http://example.com/recording.js",
-    transcriptionOutURI: "temphtaddress"; //replace this
+    transcriptionOutURI: "https://cadencecares.me/stt_out", //replace this
 });
