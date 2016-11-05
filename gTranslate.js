7c9f49aaaabd6b931570dbc42bdcbac2ec21d392
darryl3736428 <darryl3736428@gmail.com>
2016-11-05 12:09:12 -0500

    Add files via upload

---
+++ gTranslate.js
@@ -0,0 +1,39 @@
+/* Integrating TROPO API
+var tropowebapi = require('tropo-webapi');
+*/
+
+//Integrating GOOGLE TRANSLATE API
+var googleTranslate = require('google-translate')("AIzaSyAmL_0KKXcnDJKTosRC_3hD5t-GWfiVzAU");
+
+var input = "Hello World!";
+var sourceLang = "en";
+var targetLang = "zh-CN";
+var callback = function(err, translation){console.log(translation);};
+
+googleTranslate.translate(input, sourceLang, targetLang, callback)
+{
+	/* The Translate API uses an API key for authentication. This sample looks for the key in an environment variable.
+	const translate = Translate({key: process.env.TRANSLATE_API_KEY});
+	*/
+	
+	// Translates the text into the target language. "input" can be a string for translating a single piece of text, \
+	// or an array of strings for translating multiple texts.
+	googleTranslate.translate(input, sourceLang, targetLang, (err, translation) => {
+		if (err) {
+			callback(err);
+			return;
+		}
+
+    console.log('Text: %j', input);
+    console.log('Translation: %j', translation);
+    callback();
+  });
+}
+
+
+/*
+var doctorInfo = "";
+var patientInfo = "";
+var queryString = 
+
+*/
