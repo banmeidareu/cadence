5a2d8befbfdc9397d5520379d768bbce18fbe277
Jiachen Wang <Jiachen775@gmail.com>
2016-11-05 17:41:06 -0400

    frontend

---
+++ FrontEnd/dist/js/styleswitch.js
@@ -0,0 +1,6 @@
+function chooseStyle(styletitle, days) { //Interface function to switch style sheets plus save "title" attr of selected stylesheet to cookie
+if (document.getElementById){
+setStylesheet(styletitle)
+setCookie("mysheet", styletitle, days)
+}
+}
