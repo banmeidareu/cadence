616ffcdaa99fa74d3a70d1176ef77d4d63ef7a88
Jiachen Wang <Jiachen775@gmail.com>
2016-11-05 17:55:09 -0400

    https to http

--- STT.js
+++ STT.js
@@ -1,5 +1,5 @@
 record({
     beep = true,
     // probably don't need this: recordURI: "http://example.com/recording.js",
-    transcriptionOutURI: "https://cadencecares.me/stt_out", //replace this
+    transcriptionOutURI: "http://cadencecares.me/stt_out", //replace this
 });
